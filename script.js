1b08b82a6e8b5b52749cbd8f02b7b84cfdca41a9
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-07-02 10:35:41 -0700

    Add operator buttons functionality and fix some backspace and clear bugs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let calcArray = [];
-let validKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '*', 'x', '/', '%', '^', 'Backspace', '=', "Enter"];
+let validKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '*', 'x', '×', '/', '÷', '%', '^', 'Backspace', '=', "Enter"];
 let decimalEntered = false;
 let equalsEntered = false;
 
@@ -18,18 +18,18 @@ function keyInput(e) {
     if (validKeys.indexOf(e.key) != -1) { // make sure key entry is a valid calculator key
         if (validKeys.indexOf(e.key) < 11) { // was a number or decimal keyed in?
             digitInput(e.key);
+            updateDisplay(calcArray);
         } else if (validKeys.indexOf(e.key) < (validKeys.length - 3)) { // all operators
             operatorInput(e.key);
+            updateDisplay(calcArray);
         } else if (validKeys.indexOf(e.key) < (validKeys.length - 2)) { // Enable Backspace
-            console.log('Array Length: ', calcArray.length);
-            calcArray[calcArray.length - 1] = calcArray[calcArray.length - 1].slice(0, -1);
-            if (calcArray[calcArray.length - 1] === '') calcArray.pop();
+            backspace();
         } else { // it's an '=' or 'Enter' and we need to calculate
             console.log("Will peform calculation now");
             updateResult('Coming soon...'); 
+            equalsEntered = true;
         }
-        console.log(calcArray); // just for testing purposes
-        updateDisplay(calcArray);
+        //updateDisplay(calcArray) cannot go here due to backspace and allClear resetting screen
     }
 }
 
@@ -42,28 +42,31 @@ for (let i = 0; i < buttons.length; i++){
 function mouseInput() {
     if (this.className === 'digits') {
         digitInput(this.value);
+        updateDisplay(calcArray);
     } else if (this.className === 'operators') {
         // Logic for buttons that are not in validKeys array
-         if (validKeys.indexOf(this.value) > (validKeys.length - 11) && validKeys.indexOf(this.value) < (validKeys.length - 3)) { // all ValidKeys operators
-            operatorInput(this.value);
-        } else if (this.value === '') {
+         if (validKeys.indexOf(this.value) > 10 && validKeys.indexOf(this.value) < (validKeys.length - 3)) { // all ValidKeys operators
+             operatorInput(this.value);
+             updateDisplay(calcArray);
+        } else if (this.value === '.15') {
+
+        } else if (this.value === '.2') {
+
+        } else if (this.value === '√') {
+
+        } else if (this.value === '±') {
 
         }
+    } else if (this.className === 'allclear') {
+        allClear();
+    } else if (this.className === 'clear') {
+        backspace();
+    } else if (this.className === 'equals') {
+        console.log("Will peform calculation now");
+        updateResult('Coming soon...'); 
+        equalsEntered = true;
     }
-
-
-
-    updateDisplay(calcArray);
-    /*
- allclear
-    clear
-    operators
-    digits 
-    decimal
-    equals
-    */
-
-
+    //updateDisplay(calcArray) cannot go here as backspace and allClear reset display
 }   
 
 function digitInput(num) {
@@ -74,7 +77,7 @@ function digitInput(num) {
 
 function operatorInput(oper) {
     // Check to see if previous key was an operator, if so, replace it.
-    if (validKeys.indexOf(calcArray[calcArray.length - 1]) > 10 && validKeys.indexOf(calcArray[calcArray.length - 1]) < 17) {
+    if (validKeys.indexOf(calcArray[calcArray.length - 1]) > 10 && validKeys.indexOf(calcArray[calcArray.length - 1]) < (validKeys.length - 3)) {
         calcArray.pop();
     }
     calcArray.push(oper);
@@ -93,7 +96,25 @@ function calcArrayConcat(arr) {
     return arr;
 }
 
+function backspace() {
+    calcArray[calcArray.length - 1] = calcArray[calcArray.length - 1].slice(0, -1);
+    updateDisplay(calcArray);
+    if (calcArray[calcArray.length - 1] === ''){
+        calcArray.pop();
+        updateDisplay(calcArray);
+    }
+    if (calcArray.length === 0) updateDisplay(['0']);
+}
+
+function allClear() {
+    calcArray = [];
+    updateDisplay(['Enter equation']);
+    updateResult(['']);
+}
+
 function updateDisplay(arr) {
+    console.log(arr);
+    console.log(arr.join(' '));
     let equation = document.getElementById('user-input');
     equation.innerText = arr.join(' ');
 }
@@ -101,7 +122,6 @@ function updateDisplay(arr) {
 function updateResult(num) {
     let result = document.getElementById('result');
     result.innerText = '= ' + num;
-    equalsEntered = true;
 }
 
 function checkIfEqualsEntered() {
@@ -109,10 +129,4 @@ function checkIfEqualsEntered() {
         allClear();
         equalsEntered = false;
     }
-}
-
-function allClear() {
-    calcArray = [];
-    updateDisplay(['Enter equation']);
-    updateResult(['']);
 }
